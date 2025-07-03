cf0d7d4208b1c11abfa87103b40bf710aeca7271
Kristen Waterford <123219300+KWATER2352@users.noreply.github.com>
2025-07-02 23:57:41 -0700

    four.asm

--- four.asm
+++ four.asm
@@ -7,25 +7,19 @@ section .text
     global _start
 
 _start:
-    ; load variables into registers
     mov eax, [var]
     mov ebx, [var2]
 
-    ; subtract 3 from var2
     sub ebx, 3
 
-    ; multiply var by 2
     mov ecx, 2
     imul eax, ecx
 
-    ; divide var by var2-3
     cdq
     idiv ebx
 
-    ; store the result
     mov [result], eax
 
-    ; exit program
     mov eax, 1
     xor ebx, ebx
     int 0x80
